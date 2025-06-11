741031b0981a9c2918ba66b10bcd948028e88538
Your Name <yeshwanthvenkat.u@gmail.com>
2025-06-11 12:25:37 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,42 @@
 import Link from "next/link";
 
-import { LatestPost } from "~/app/_components/post";
-import { api, HydrateClient } from "~/trpc/server";
+
+import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+ 
 
-  void api.post.getLatest.prefetch();
+  
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
+      <main className="bg-gradient-to-br from-[#FAE500] to-[#FF0000] min-h-screen ">
+        <header className="flex justify-between  bg-gradient-to-r from-orange-400 to yellow-100 shadow-lg boreder-b-2">
+          <div className="items-center text-4xl font-bold text-green-900 p-4">
+            <h1>VNMills</h1>
           </div>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
+          <div className="flex items-center gap-4 p-4 text-xl text-black">
+            <p>more</p>
+            <p>contact us</p>
           </div>
 
-          <LatestPost />
-        </div>
+        </header>
+        <section >
+          <div className="flex justify-center gap-2 p-4 ">
+            <img
+            className="rounded-lg shadow-lg "
+             alt="image" 
+             src="https://media.istockphoto.com/photos/landscape-shot-rice-mill-with-reflection-and-sunset-picture-id471247018?k=6&m=471247018&s=612x612&w=0&h=TgWGpWiLGa_VWNtK-xTwS3kMxDyJqRZkV5qe1YFnGik=" />
+             <div className="  p-4 border-2 shadow-lg gap-2">
+              <h1 className=" text-4xl text-center text-brown-400 font-bold p-4 mb-2">Welcome to VNMills</h1>
+               <p className="flex text-justify text-green-800 text-xl p-4 ">We are a leading rice milling company dedicated to delivering high-quality rice products 
+              through advanced technology, strict quality control, and sustainable practices. Our mill processes a wide variety of rice, including Basmati,
+               non-Basmati, parboiled, and organic varieties, tailored for both domestic and international markets.</p>
+             </div>
+            
+          </div>
+        </section>
+          
       </main>
     </HydrateClient>
   );
